9175838ce5436161b6d66d489ae309cf27cbb7b9
NiuZhiYong <niuzhiyong1013@126.com>
2018-07-04 14:25:26 +0800

    header and content

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,3 +3,32 @@ import PropTypes from 'prop-types'
 import ThemeSwitch from './themeSwitch'
 import {connect} from './react-redux'
 
+class Content extends Component {
+    static propTypes = {
+        store:PropTypes.object
+    }
+    constructor(){
+        super();
+        this.state = {
+            themeColor:''
+        }
+    }
+    componentWillMount(){
+        this._updateThemeColor()
+    }
+    _updateThemeColor(){
+        const {store} = this.context;
+        const state = store.getState();
+        this.setState({
+            themeColor:state.themeColor
+        })
+    }
+    render(){
+        return (
+            <div>
+                <p style = {{color:this.state.themeColor}}>React.js----Book.content</p>
+                <ThemeSwitch />
+            </div>
+        )
+    }
+}
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,3 +2,29 @@ import React,{Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
+class Header extends Component {
+    static contextTypes = {
+        store:PropTypes.object
+    }
+    constructor(){
+        super();
+        this.state = {
+            themeColor:''
+        }
+    }
+    componentWillMount(){
+        this._updateThemeColor()
+    }
+    _updateThemeColor(){
+        const {store} = this.context;
+        const state = store.getState()
+        this.setState({
+            themeColor:state.themeColor
+        })
+    }
+    render(){
+        return (
+            <h1 style={{color:this.state.themeColor}}>React.js------Book</h1>
+        )
+    }
+}
