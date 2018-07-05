b91c618c330f77541f94ad0f626b9e2caab72867
NiuZhiYong <niuzhiyong1013@126.com>
2018-07-05 09:43:25 +0800

    connect

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,31 +2,23 @@ import React,{Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-export default class Header extends Component {
-    static contextTypes = {
-        store:PropTypes.object
-    }
-    constructor(){
-        super();
-        this.state = {
-            themeColor:''
-        }
-    }
-    componentWillMount(){
-        const {store} = this.context;
-        this._updateThemeColor();
-        store.subscribe(() => this._updateThemeColor())
-    }
-    _updateThemeColor(){
-        const {store} = this.context;
-        const state = store.getState()
-        this.setState({
-            themeColor:state.themeColor
-        })
+class Header extends Component {
+    static propTypes = {
+        themeColor:PropTypes.string
     }
     render(){
         return (
-            <h1 style={{color:this.state.themeColor}}>React.js------Book</h1>
+            <h1 style={{color:this.props.themeColor}}>React.js------Book</h1>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        themeColor:state.themeColor
+    }
+}
+
+Header = connect(mapStateToProps)(Header)
+
+export default Header
